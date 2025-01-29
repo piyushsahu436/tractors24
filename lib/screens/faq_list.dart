8637372d981f4b2e78c5d0fecf283548892c9326
avani jain <120156220+avanijain1331@users.noreply.github.com>
2025-01-29 18:44:57 +0530

    contactseller , buyers form , icons modified

--- lib/screens/faq_list.dart
+++ lib/screens/faq_list.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class FAQScreen extends StatefulWidget {
-  const FAQScreen({super.key});
+class FAQList extends StatefulWidget {
+  const FAQList({super.key});
 
   @override
-  _FAQScreenState createState() => _FAQScreenState();
+  _FAQListState createState() => _FAQListState();
 }
 
-class _FAQScreenState extends State<FAQScreen> {
-
-    int? expandedSectionIndex;
+class _FAQListState extends State<FAQList> {
+  int? expandedSectionIndex;
   int? expandedQuestionIndex;
 
-  // FAQ Data
   final List<Map<String, dynamic>> faqData = [
     {
       'title': 'For Buyers',
@@ -89,121 +87,22 @@ class _FAQScreenState extends State<FAQScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            height: size.height * 0.3,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/Vector3.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 39), // Add space from top for status bar
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
-                          size: 24,
-                        ),
-                      ),
-                      SizedBox(width: 115), // Space between icon and text
-                      Text(
-                        'FAQ`S',
-                        style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Image(
-                        image: AssetImage('assets/images/img.png'),
-                        width: size.width * 0.8,
-                        height: size.height * 0.8,
-                        fit: BoxFit.scaleDown,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          _buildHeader(),
-          Container(
-            child: Expanded(
-
-              child: _buildFAQList(),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildHeader() {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Center(
-            child: Text(
-              'We are here to help you',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                //fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            'Questions commonly asked by Buyers and Sellers',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey[600],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildFAQList() {
     return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
       padding: const EdgeInsets.all(16),
       itemCount: faqData.length,
       itemBuilder: (context, sectionIndex) {
         return Card(
           margin: const EdgeInsets.only(bottom: 35),
-          elevation: 2,
+          elevation: 3,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
           child: Column(
             children: [
               _buildSectionHeader(sectionIndex),
-              if (expandedSectionIndex == sectionIndex)
-                _buildQuestionsList(sectionIndex),
+              if (expandedSectionIndex == sectionIndex) _buildQuestionsList(sectionIndex),
             ],
           ),
         );
@@ -215,11 +114,7 @@ class _FAQScreenState extends State<FAQScreen> {
     return InkWell(
       onTap: () {
         setState(() {
-          if (expandedSectionIndex == sectionIndex) {
-            expandedSectionIndex = null;
-          } else {
-            expandedSectionIndex = sectionIndex;
-          }
+          expandedSectionIndex = expandedSectionIndex == sectionIndex ? null : sectionIndex;
           expandedQuestionIndex = null;
         });
       },
@@ -229,9 +124,7 @@ class _FAQScreenState extends State<FAQScreen> {
           color: Colors.white,
           borderRadius: BorderRadius.vertical(
             top: const Radius.circular(12),
-            bottom: Radius.circular(
-              expandedSectionIndex == sectionIndex ? 0 : 12,
-            ),
+            bottom: Radius.circular(expandedSectionIndex == sectionIndex ? 0 : 12),
           ),
         ),
         child: Row(
@@ -239,16 +132,12 @@ class _FAQScreenState extends State<FAQScreen> {
           children: [
             Text(
               faqData[sectionIndex]['title'],
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+              style: GoogleFonts.anybody(
+                  color: Color(0xFF414040),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
             ),
-            Icon(
-              expandedSectionIndex == sectionIndex
-                  ? Icons.keyboard_arrow_up
-                  : Icons.keyboard_arrow_down,
-            ),
+            Icon(expandedSectionIndex == sectionIndex ? Icons.keyboard_arrow_up_rounded : Icons.arrow_drop_down_circle_outlined),
           ],
         ),
       ),
@@ -264,8 +153,7 @@ class _FAQScreenState extends State<FAQScreen> {
         return Column(
           children: [
             _buildQuestionTile(sectionIndex, questionIndex),
-            if (expandedQuestionIndex == questionIndex)
-              _buildAnswerTile(sectionIndex, questionIndex),
+            if (expandedQuestionIndex == questionIndex) _buildAnswerTile(sectionIndex, questionIndex),
           ],
         );
       },
@@ -276,23 +164,14 @@ class _FAQScreenState extends State<FAQScreen> {
     return InkWell(
       onTap: () {
         setState(() {
-          if (expandedQuestionIndex == questionIndex) {
-            expandedQuestionIndex = null;
-          } else {
-            expandedQuestionIndex = questionIndex;
-          }
+          expandedQuestionIndex = expandedQuestionIndex == questionIndex ? null : questionIndex;
         });
       },
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: const BoxDecoration(
           color: Color(0xFFE3F2FD),
-          border: Border(
-            bottom: BorderSide(
-              color: Colors.white,
-              width: 1,
-            ),
-          ),
+          border: Border(bottom: BorderSide(color: Colors.white, width: 1)),
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -300,16 +179,13 @@ class _FAQScreenState extends State<FAQScreen> {
             Expanded(
               child: Text(
                 faqData[sectionIndex]['questions'][questionIndex]['question'],
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
+                style: GoogleFonts.anybody(
+                    color: Color(0xFF414040),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500),
               ),
             ),
-            Icon(
-              expandedQuestionIndex == questionIndex ? Icons.remove : Icons.add,
-              color: Colors.blue,
-            ),
+            Icon(expandedQuestionIndex == questionIndex ? Icons.remove : Icons.add, color: Colors.blue),
           ],
         ),
       ),
@@ -322,10 +198,10 @@ class _FAQScreenState extends State<FAQScreen> {
       color: const Color(0xFFBBDEFB),
       child: Text(
         faqData[sectionIndex]['questions'][questionIndex]['answer'],
-        style: const TextStyle(
-          fontSize: 16,
-          color: Colors.black87,
-        ),
+        style: GoogleFonts.anybody(
+            color: Color(0xFF414040),
+            fontSize: 12,
+            fontWeight: FontWeight.w400),
       ),
     );
   }
