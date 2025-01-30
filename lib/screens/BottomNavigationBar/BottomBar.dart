13ba77b98b4b1c8232ce8d8e6300a604ef4f6704
avani jain <120156220+avanijain1331@users.noreply.github.com>
2025-01-30 11:00:11 +0530

    policy screeen , loan enquiry screen

--- lib/screens/BottomNavigationBar/BottomBar.dart
+++ lib/screens/BottomNavigationBar/BottomBar.dart
@@ -14,6 +14,7 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
+      color: Colors.transparent,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
