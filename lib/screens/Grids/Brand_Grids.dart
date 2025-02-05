edb5dc99a3ead52cd44aeaf27a1ad9745706cc25
YashRaj107 <122400210+YashRaj107@users.noreply.github.com>
2025-02-05 10:43:38 +0530

    changes from yash

--- lib/screens/Grids/Brand_Grids.dart
+++ lib/screens/Grids/Brand_Grids.dart
@@ -16,10 +16,38 @@ class BrandGrids extends StatelessWidget {
     GridItem(imageUrl: 'assets/images/mahindraL.png', title: 'Mahindra'),
     GridItem(imageUrl: 'assets/images/PreetL.png', title: 'Preet Tractors'),
     GridItem(imageUrl: 'assets/images/SonalikaL.png', title: 'Sonalika'),
-    GridItem(imageUrl: 'assets/images/JohnDeerL.png', title: 'John Deer'),
-    GridItem(imageUrl: 'assets/images/mahindraL.png', title: 'Mahindra'),
-    GridItem(imageUrl: 'assets/images/PreetL.png', title: 'Preet Tractors'),
-    GridItem(imageUrl: 'assets/images/SonalikaL.png', title: 'Sonalika'),
+    GridItem(imageUrl: 'assets/images/marut.jpg', title: 'SMEA'),
+    GridItem(imageUrl: 'assets/images/a-c-e-.jpg', title: 'ACE'),
+    GridItem(imageUrl: 'assets/images/agri-king.jpg', title: 'Agri King'),
+    GridItem(imageUrl: 'assets/images/autonxt.jpg', title: 'AutoNxt'),
+    GridItem(imageUrl: 'assets/images/captain.jpg', title: 'Captain'),
+    GridItem(imageUrl: 'assets/images/cellestial.jpg', title: 'Cellestial'),
+    GridItem(imageUrl: 'assets/images/eicher.jpg', title: 'Eicher'),
+    GridItem(imageUrl: 'assets/images/escorts.jpg', title: 'Escorts'),
+    GridItem(imageUrl: 'assets/images/farmtrac.jpg', title: 'FARMTRAC'),
+    GridItem(imageUrl: 'assets/images/force.jpg', title: 'Force'),
+    GridItem(imageUrl: 'assets/images/hav.jpg', title: 'Hav'),
+    GridItem(imageUrl: 'assets/images/hindustan.jpg', title: 'Hindustan'),
+    GridItem(imageUrl: 'assets/images/indo-farm.jpg', title: 'Indo Farm Equipment Ltd.'),
+    GridItem(imageUrl: 'assets/images/kartar.jpg', title: 'Kartar'),
+    GridItem(imageUrl: 'assets/images/kubota.jpg', title: 'Kubota'),
+    GridItem(imageUrl: 'assets/images/massey-ferguson.jpg', title: 'Massey Ferguson'),
+    GridItem(imageUrl: 'assets/images/maxgreen.jpg', title: 'Maxgreen'),
+    GridItem(imageUrl: 'assets/images/montra.jpg', title: 'Montra'),
+    GridItem(imageUrl: 'assets/images/new-holland.jpg', title: 'New Holland'),
+    GridItem(imageUrl: 'assets/images/powertrac.jpg', title: 'Power Trac'),
+    GridItem(imageUrl: 'assets/images/same-deutz-fahr.jpg', title: 'Same Deutz Fahr'),
+    GridItem(imageUrl: 'assets/images/solis.jpg', title: 'Solis'),
+    GridItem(imageUrl: 'assets/images/standard.jpg', title: 'Standard'),
+    GridItem(imageUrl: 'assets/images/swaraj.png.jpg', title: 'Swaraj'),
+    GridItem(imageUrl: 'assets/images/trackstar.jpg', title: 'TRAKSTAR'),
+    GridItem(imageUrl: 'assets/images/valdo.jpg', title: 'Valdo'),
+    GridItem(imageUrl: 'assets/images/autonxt.jpg', title: 'AutoNxt'),
+    GridItem(imageUrl: 'assets/images/autonxt.jpg', title: 'AutoNxt'),
+    GridItem(imageUrl: 'assets/images/autonxt.jpg', title: 'AutoNxt'),
+    GridItem(imageUrl: 'assets/images/autonxt.jpg', title: 'AutoNxt'),
+
+
   ];
 
   @override
@@ -33,7 +61,7 @@ class BrandGrids extends StatelessWidget {
           crossAxisCount: 4, // Number of columns
           crossAxisSpacing: 10.0, // Spacing between columns
           mainAxisSpacing: 20.0, // Spacing between rows
-          childAspectRatio: 0.9, // Aspect ratio of each item
+          childAspectRatio: 0.7, // Aspect ratio of each item
         ),
         itemCount: gridItems.length, // Number of items
         itemBuilder: (context, index) {
@@ -50,26 +78,33 @@ class BrandGrids extends StatelessWidget {
                 ),
               ],
             ),
-            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(5.0),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(8),
-                    ),
-                    child: Image.asset(
-                      item.imageUrl,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
+                  child: Expanded(
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(8),
+                      ),
+                      child: Image.asset(
+                        item.imageUrl,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
                     ),
                   ),
                 ),
-                Text(item.title,style: GoogleFonts.anybody(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 8
-                ),),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Expanded(
+                    child: Text(item.title,style: GoogleFonts.anybody(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 8
+                    ),),
+                  ),
+                ),
               ],
             )
           );
